6f3735c8700cfbca3e3b829f46918524848f5755
sujalcharati <sujalc024@gmail.com>
2024-12-31 16:50:31 +0530

    homepage_flex

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,9 @@ interface HeaderProps {
   }
 export const Header: React.FC<HeaderProps> = ({ style }) => {
     return ( 
-        <div className="text-white bg-transparent" style={style}>
+        <div className="text-white" style={style}>
             <nav>
-            <ul className="flex list-none p-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+            <ul className="flex items-center justify-center list-none p-0" style={{ backgroundColor: 'black', fontSize: '18px' }}>
             <li className="mr-5"><a href="#home">Home</a></li>
             <li className="mr-5"><a href="#projects">Projects</a></li>
             <li className="mr-5"><a href="#resume">Resume</a></li>
---
+++ app/components/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Homepage =()=>{
+    return(
+        <div className="flex justify-center items-center text-white bg-black bg-opacity-0 backdrop-filter backdrop-blur-lg rounded-lg shadow-md w-1/2 h-52 fixed inset-0 m-auto p-4">
+          
+            Welcome to my portfolio! Here you will find information about my projects, skills, and experience. Feel free to explore and learn more about my work.
+            
+        </div>
+    )
+}
