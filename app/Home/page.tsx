db13d52533ecc625ba6150e845c08b7a48215ddd
sujalcharati <sujalc024@gmail.com>
2025-01-17 21:16:41 +0530

    floating_dock

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { Contact } from "../components/Contact";
+import React from 'react';
+import { FloatingDockDemo } from "../components/FloatingDockDemo";
 import { Header } from "../components/Header"
 import {  Homepage  } from "../components/Homepage"
 import ThreeScene from "../components/ThreeScene"
@@ -9,10 +10,9 @@ export default function Home(){
     <div style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden' }}>
         <ThreeScene style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }} />
         <Header style={{ position: 'relative', zIndex: 1, backgroundColor: 'rgba(255, 255, 255, 0.8)' }} />
-        <Header />
     </div>
     <Homepage />
-    <Contact/>
+   <FloatingDockDemo/>
 </div>
     );
 
